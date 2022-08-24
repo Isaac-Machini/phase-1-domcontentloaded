6b755aeea752c6ce92a66d9c406d26ede6cd0ea5
Isaac-Machini <isaac.machini@student.moringaschool.com>
2022-08-24 16:55:26 +0300

    complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Your code goes here
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    function updateP(text){
+        let p = document.querySelector( 'p' )
+        p.textContent = text
+    }
+ 
+    updateP('This is really cool!')
+})
